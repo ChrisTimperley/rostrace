ad2becb1d28f44a6b2033395e098e5dc0a793a90
Chris Timperley <christimperley@googlemail.com>
2017-04-06 13:38:26 -0400

    modified setup file

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@
 from setuptools import setup
 
 setup(
-    name='rosbagtotrace',
+    name='rostrace',
     version='0.0.1',
     description='Converts ROS bag files into Daikon traces and declarations',
     long_description='TBA',
     # need to modify to have multiple authors!
-    author='Afsoon Afsal',
-    # TODO: add author email and repo URL
-    #author_email='
-    #url='https://github.com/ChrisTimperley/roshammer',
+    author='Afsoon Afsal, Chris Timperley',
+    author_email='afsafzal@cs.cmu.edu, christimperley@gmail.com',
+    # TODO: move to Squares organisation
+    url='https://github.com/ChrisTimperley/rostrace',
     license='mit',
-    packages=['rosbagtotrace'],
+    packages=['rostrace'],
     entry_points = {
-        'console_scripts': [ 'rosbagtotrace = rosbagtotrace.rosbagtotrace:main' ]
+        'console_scripts': [ 'rostrace = rostrace.rostrace:main' ]
     }
 )
