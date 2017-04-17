7417444beb41e24a8c7cd8380b6a4ce0f05f197f
ChrisTimperley <christimperley@googlemail.com>
2017-04-17 14:57:07 -0400

    working on message flatenning

--- rostrace/reader.py
+++ rostrace/reader.py
@@ -3,25 +3,40 @@
 # This file is responsible for reading data from a given ROS bag file and
 # transforming it into a set of program points, usable by Daikon
 #
-# - header is only included in some messages
-# - messages are encoded in YAML
-
 import sys
 import rosbag
 import yaml
 
 from pprint import pprint as pp
 
+# Flattens a set of nested dictionaries into a single dictionary
+
 # Writes to a dtrace file
 def write_to_dtrace(data):
     pass
+
+def extract_declaration_for_
+
+# Flattens the contents of a (decoded) message into a dictionary from flattened
+# to values.
+#def flatten_message_content(msg, prefixes=[]):
+#    if not isinstance(msg, dict):
+#        return {}
+#    
+#    out = {}
+#    for (name, val) in msg.items():
+#        if isinstance(val, dict):
+#            out += flatten_message_content(msg, prefixes + [name])
+#        elif isinstance(val, list):
+#
+#    return out
 
 # Extracts all variable values from a given message into a dictionary of the
 # form {name: value}, where names are flattened into a one-dimensional form
 # (e.g. pose.position.x).
 #
 # Returns None if there was an error whilst reading the message.
-def extract_variable_values_from_message(topic, msg):
+def extract_vars_from_message(topic, msg):
     msg = str(msg)
 
     # A bug in the YAML output formatting of certain ROS messages can cause
@@ -30,18 +45,17 @@ def extract_variable_values_from_message(topic, msg):
         msg = yaml.load(msg)
     except yaml.scanner.ScannerError:
         return None
-
-    # Flatten the YAML file
-
+    
     return {}
+
+# def get_message_format(msg):
 
 def convert_bag_to_program_points(filename):
     bag = rosbag.Bag(filename)
     for entry in bag:
-        # entry.topic
-        # entry.message
+        var_vals = extract_vars_from_message(entry.topic, entry.message)
 
-        var_vals = extract_variable_values_from_message(entry.topic, entry.message)
+        sys.exit(0)
 
         # pp(var_vals)
 
