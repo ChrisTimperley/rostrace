2d5d5a3e3871e2c10c44b910e735829ff168d9e6
ChrisTimperley <christimperley@googlemail.com>
2017-04-19 13:57:01 -0400

    tidied up service tapper

--- rostrace/service_recorder.py
+++ rostrace/service_recorder.py
@@ -4,6 +4,9 @@
 #       - we could use some catkin and ROS to find it and dynamically
 #           import it
 #
+# Limitations:
+# - doesn't work if another node is using a persistent connection
+#
 # http://wiki.ros.org/ROS/Tutorials/WritingServiceClient%28python%29
 import rospy
 import std_srvs.srv
@@ -34,34 +37,47 @@ whilst logging details of the service call to the appropriate topic
 #    except rospy.ServiceException, e:
 #        print("Service call failed: {}".format(e))
 
-def handle(proxy, req):
+def handle(pub, proxy, req):
+    rospy.Publisher('/rec/srvs', std_msgs.msg.String)
+
     print("[poison]")
     ret = proxy(req)
     print("[/poison]")
     return ret
 
-def server():
-    master = rosgraph.Master('/service_tap')
-    rospy.init_node('service_tap')
+"""
+Installs a tap on a given service, causing all activity on that service to be
+logged to the /rec/srvs topic.
+"""
+def tap_service(service_name):
+    # TODO
+    service_msg_format = std_srvs.srv.Empty
 
-    # create a proxy to the original service
-    rospy.wait_for_service('hello_world')
-    proxy = rospy.ServiceProxy('hello_world', std_srvs.srv.Empty, persistent=True)
+    # block until the service is available
+    rospy.wait_for_service(service_name)
 
-    # create the transport for this proxy
+    # create a persistent proxy to that service
+    # inject a persistent connection into the proxy, so that when we replace
+    # the original service, we can still forward messages onto the old one
+    proxy = rospy.ServiceProxy(service_name, service_msg_format, persistent=True)
+
     # TODO: listen for failures
     # http://docs.ros.org/jade/api/rospy/html/rospy.impl.tcpros_service-pysrc.html#ServiceProxy
+    master = rosgraph.Master('/service_tap')
     service_uri = master.lookupService(proxy.resolved_name)
     (dest_addr, dest_port) = rospy.core.parse_rosrpc_uri(service_uri)
     proxy.transport = TCPROSTransport(proxy.protocol, proxy.resolved_name) 
     proxy.transport.buff_size = proxy.buff_size
     proxy.transport.connect(dest_addr, dest_port, service_uri) 
 
-    # TODO: remember to close the connection!
+    # create a new, tapped service, with the same name
+    rospy.Service('hello_world', std_srvs.srv.Empty, (lambda r: handle(pub, proxy, r)))
 
-    # create a new service
-    rospy.Service('hello_world', std_srvs.srv.Empty, (lambda r: handle(proxy, r)))
+def tap_services(services):
+    rospy.init_node('/service_tap')
+    for service in services:
+        tap_service(service)
     rospy.spin()
 
 if __name__ == "__main__":
-    server()
+    tap_services(['hello_world'])
