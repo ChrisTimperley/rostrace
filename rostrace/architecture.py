a50b404cb22dec13710af57c3b056bf307ab7c22
ChrisTimperley <christimperley@googlemail.com>
2017-04-19 16:05:09 -0400

    updated architecture logging

--- rostrace/architecture.py
+++ rostrace/architecture.py
@@ -1,10 +1,12 @@
-#!/usr/bin/python3
-#
-#
+#!/usr/bin/env python
 import rospy
 import rostopic
+import std_msgs.msg
+import json
 
-from subprocess import Popen, devnull
+from subprocess import Popen
+from os import devnull
+
 
 """
 This function is responsible for implementing a ROS node which periodically
@@ -13,11 +15,10 @@ publishes the state of the architecture to the "rec/architecture" topic.
 Each message published to this topic describes the topics to which a given node
 is currently publishing.
 """
-def record_publishers():
-    # TODO: why queue_size=10? Surely queue_size=1 is more appropriate?
-    pub = rospy.Publisher('rec/architecture_publishers', String, queue_size=10)
-    rospy.init_node('architecture_publishers')
-    rate = rospy.Rate(10) # 10 Hz (TODO: excessive?)
+def record():
+    pub = rospy.Publisher('rec/arch', std_msgs.msg.String, queue_size=5)
+    rospy.init_node('rostrace_architecture')
+    rate = rospy.Rate(1) # 10 Hz (TODO: excessive?)
 
     previous_state = {}
     while not rospy.is_shutdown():
@@ -29,23 +30,27 @@ def record_publishers():
         # get the current architecture state
         current_state = {}
         for (topic, _) in topics:
-            (pubs, subs) = get_publishers_and_subscribers(topic)
+            (pubs, subs) = publishers_and_subscribers(topic)
             current_state[topic] = {
                 'publishers': pubs,
                 'subscribers': subs
             }
 
+        if previous_state != current_state:
+            pub.publish(json.dumps(current_state))
+            previous_state = current_state
+
         # find, and publish, any differences with the previous state
         # TODO
-        if last_publish != new_publish:
-            p = {}
-            for topic in new_publish.keys():
-                p[topic] = list(new_publish[topic])
-            last_publish = new_publish
-            pub.publish(str(p))
+#        if last_publish != new_publish:
+#            p = {}
+#            for topic in new_publish.keys():
+#                p[topic] = list(new_publish[topic])
+#            last_publish = new_publish
+#            pub.publish(str(p))
 
-        previous_state = current_state
         rate.sleep()
+
 
 """
 Returns a tuple of sets, containing the names of the subscribers and publishers
@@ -59,32 +64,34 @@ def publishers_and_subscribers(topic):
     info = info[3:]
 
     # find the line where the subscribers list begins
-    subscribers_at = info.index('Subscribers:')
+    subscribers_at = next((l for l in info if l.startswith('Subscribers:')), -1)
+    subscribers_at = info.index(subscribers_at)
     assert subscribers_at != -1
 
     # get a list of pubs and subs
     get_name = lambda l: l.split(' ')[1]
-    publishers = set(get_name(pub) for pub in info[:subscribers_at-1])
-    subscribers = set(get_name(sub) for sub in info[subscribers_at:-1])
+    publishers = [get_name(pub) for pub in info[:subscribers_at-1]]
+    subscribers = [get_name(sub) for sub in info[subscribers_at+1:-1]]
 
     return (publishers, subscribers)
+
 
 if __name__ == "__main__":
 
     # start recording publishers (in another thread)
-    record_publishers()
+    record()
 
     # start recording ROS bag
     # TODO: for now, we record ALL topics
     #       in the future, we should record specific topics
-    cmd = "rosbag record -a"
-    with Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, preexec_fn=preexecute, cwd=sandboxd) as p:
-
-        # TODO: listen out for bad return codes and errors
-        try:
-            p.communicate(timeout=tlim)
-
-        # TODO: be specific about what sort of exceptions we want to stop
-        # recording
-        except:
-            os.killpg(p.pid, signal.SIGKILL)
+#    cmd = "rosbag record -a"
+#    with Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, preexec_fn=preexecute, cwd=sandboxd) as p:
+#
+#        # TODO: listen out for bad return codes and errors
+#        try:
+#            p.communicate(timeout=tlim)
+#
+#        # TODO: be specific about what sort of exceptions we want to stop
+#        # recording
+#        except:
+#            os.killpg(p.pid, signal.SIGKILL)
