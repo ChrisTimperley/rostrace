4adabfd88b28d0ee7224cf6eb23eec50f74fad31
ChrisTimperley <christimperley@googlemail.com>
2017-04-19 14:56:31 -0400

    service tap is now generic

--- rostrace/service_recorder.py
+++ rostrace/service_recorder.py
@@ -2,28 +2,60 @@
 #
 # Limitations:
 # - doesn't work if another node is using a persistent connection
+# - all names MUST be fully qualified, else rosservice will fail
+#
+# http://docs.ros.org/diamondback/api/rosservice/html/index.html
+
+import inspect
 import rospy
 import std_srvs.srv
 import std_msgs.msg
 import rosgraph
+import rosservice
 import rospy.core
+import json
+
+from pprint import pprint as pp
 
 from rospy.impl.tcpros_base import TCPROSTransport
 
+# we use the most accurate timer available to the system
+from timeit import default_timer as timer
+
 """
 All (tapped) service calls are broadcast to the /rec/srvs topic in a JSON
-format
+format. The +queue_size+ parameter creates an asynchronous publisher, which
+is better suited to our needs (higher throughput)
 """
-ServiceCallPublisher = rospy.Publisher('/rec/srvs', std_msgs.msg.String)
+serviceCallPublisher = rospy.Publisher('rec/srvs', std_msgs.msg.String, queue_size=10)
 
 """
 Acts a proxy, forwarding a given service call onto its intended recepient,
 whilst logging details of the service call to the appropriate topic
 """
-def handle(proxy, req):
+def handle(server, service_name, proxy, req):
+    time_start = timer()
+    client = req._connection_header['callerid']
+
     print("[poison]")
     ret = proxy(req)
     print("[/poison]")
+
+    # determine the response time
+    time_end = timer()
+    time_duration = time_end - time_start
+
+    # log the service call
+    log = {
+        'service': service_name,
+        'server': server,
+        'client': client,
+        'time_start': time_start,
+        'time_end': time_end,
+        'time_duration': time_duration
+    }
+    serviceCallPublisher.publish(json.dumps(log))
+
     return ret
 
 """
@@ -31,20 +63,24 @@ Installs a tap on a given service, causing all activity on that service to be
 logged to the /rec/srvs topic.
 """
 def tap_service(service_name):
-    # TODO
-    service_msg_format = std_srvs.srv.Empty
-
     # block until the service is available
     rospy.wait_for_service(service_name)
+
+    # determine which node provides the given service
+    server = rosservice.get_service_node(service_name)
+    assert not server is None
+
+    # get the class used by this service
+    service_cls = rosservice.get_service_class_by_name(service_name)
 
     # create a persistent proxy to that service
     # inject a persistent connection into the proxy, so that when we replace
     # the original service, we can still forward messages onto the old one
-    proxy = rospy.ServiceProxy(service_name, service_msg_format, persistent=True)
+    proxy = rospy.ServiceProxy(service_name, service_cls, persistent=True)
 
     # TODO: listen for failures
     # http://docs.ros.org/jade/api/rospy/html/rospy.impl.tcpros_service-pysrc.html#ServiceProxy
-    master = rosgraph.Master('/service_tap')
+    master = rosgraph.Master('service_tap')
     service_uri = master.lookupService(proxy.resolved_name)
     (dest_addr, dest_port) = rospy.core.parse_rosrpc_uri(service_uri)
     proxy.transport = TCPROSTransport(proxy.protocol, proxy.resolved_name) 
@@ -52,13 +88,14 @@ def tap_service(service_name):
     proxy.transport.connect(dest_addr, dest_port, service_uri) 
 
     # create a new, tapped service, with the same name
-    rospy.Service(service_name, service_msg_format, (lambda r: handle(proxy, r)))
+    tap = lambda r: handle(server, service_name, proxy, r)
+    rospy.Service(service_name, service_cls, tap)
 
 def tap_services(services):
-    rospy.init_node('/service_tap')
+    rospy.init_node('service_tap')
     for service in services:
         tap_service(service)
     rospy.spin()
 
 if __name__ == "__main__":
-    tap_services(['hello_world'])
+    tap_services(['/hello_world'])
