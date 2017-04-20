6f2e0c3b1234816774c657c231bc66a27b1b0ce3
ChrisTimperley <christimperley@googlemail.com>
2017-04-20 15:08:51 -0400

    fixed rostrace to start bag recording before monitoring

--- rostrace/rostrace.py
+++ rostrace/rostrace.py
@@ -11,21 +11,20 @@ import architecture
 
 def trace():
     rospy.init_node('rostrace')
-
-    # setup service re-direction
-    # blocks until all services are redirected
-    services = ['/hello_world']
-    service.tap_services(services)
-
-    # setup architecture monitoring
     try:
-        thread.start_new_thread(architecture.record, ())
-
         # use semaphore to communicate status of architecture
         # log to bag file
         p_recorder = subprocess.Popen("rosbag record -a", preexec_fn=os.setsid, shell=True)
         print("Recording to rosbag...")
         thread.start_new_thread(p_recorder.communicate, ())
+
+        # architecture monitoring
+        thread.start_new_thread(architecture.record, ())
+
+        # setup service re-direction
+        # blocks until all services are redirected
+        services = ['/hello_world']
+        service.tap_services(services)
 
         # spin!
         rospy.spin()
